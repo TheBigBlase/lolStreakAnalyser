e7e555399ae826987249a698b3fc3f4ff8f7f9ff
Andrew Mary Huet de Barochez <phoqueeberlue@gmail.com>
2022-10-30 21:36:27 +0100

    Added a cache system to prevent redundant API calls. Implemented getMatchesID and getMatchData

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from src.GameExtractor import GameExtractor
 
 if __name__ == "__main__":
-    GameExtractor = GameExtractor()
-    puuid = GameExtractor.getPuuidBySummonerName("Phoque éberlué")
-    games = GameExtractor.getSummonerMatchesID(puuid)
+    GameExtractor = GameExtractor(use_cache_file=True)
+
+    summoner_name = GameExtractor.sanitizeSummonerName("Phoque éberlué")
+
+    puuid: str = GameExtractor.getPuuidBySummonerName(summoner_name)
+    matches_id: list[str] = GameExtractor.getSummonerMatchesID(puuid, summoner_name)
+    matches_data: list[any] = GameExtractor.getMatchesData(matches_id, summoner_name)
+
--- src/GameExtractor.py
+++ src/GameExtractor.py
@@ -10,10 +10,25 @@ class GameExtractor:
     Class used to extract data from Riot Games API
     """
 
-    def __init__(self):
+    def __init__(self, use_cache_file=True):
         # load our summoner data
-        self.json_file = json.load(open("./apikey.json", "r"))
+        with open("./apikey.json", "r") as key_file:
+            self.json_file = json.load(key_file)
+
+        self.use_cache_file = use_cache_file
+
+        if use_cache_file:
+            self.cache_path = "./cache.json"
+
+            with open(self.cache_path, "r") as cache_file:
+                self.cache_data = json.load(cache_file)
+
         self.api_key = self.json_file["X-Riot-Token"]
+
+    @staticmethod
+    def sanitizeSummonerName(summoner_name: str):
+        # Replacing spaces with plus
+        return summoner_name.replace(" ", "+")
 
     @staticmethod
     def execCurl(url: str) -> any:
@@ -43,8 +58,12 @@ class GameExtractor:
         :param summoner_name: summoner name
         :return: puuid
         """
-        # Replacing spaces with plus
-        summoner_name = summoner_name.replace(" ", "+")
+
+        # Checks if the puuid is already in cache
+        if self.use_cache_file \
+                and summoner_name in self.cache_data \
+                and "puuid" in self.cache_data[summoner_name]:
+            return self.cache_data[summoner_name]["puuid"]
 
         url = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' \
               + summoner_name + '?api_key=' + self.api_key
@@ -52,17 +71,108 @@ class GameExtractor:
         # Executing API request
         res = self.execCurl(url)
         print(res)
+
+        # Saving the results in cache file if activated
+        if self.use_cache_file:
+            self.cache_data[summoner_name] = res
+
+            with open(self.cache_path, "w", encoding="utf-8") as file:
+                file.write(json.dumps(self.cache_data))
+
         return res["puuid"]
 
-    def getSummonerMatchesID(self, puuid: str) -> set[str]:
+    def getSummonerMatchesID(self, puuid: str, summoner_name: str = "Phoque éberlué") -> list[str]:
         """
         Gets the matches IDs of a given summoner
         :param puuid: the puuid of the summoner
+        :param summoner_name: the name of the summoner
+        :return: a list of match ID
+        """
+
+        # Checks if the matches id are in the cache file
+        if self.use_cache_file \
+                and "matches_id" in self.cache_data[summoner_name]:
+            return self.cache_data[summoner_name]["matches_id"]
+
+        url = f'https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids' \
+              f'?start=0' \
+              f'&count=100' \
+              f'&api_key={self.api_key}' \
+              f'&queue=420'  # Indicates that we only want ranked games
+
+        res = self.execCurl(url)
+
+        # Saves matches id in the cache file
+        if self.use_cache_file:
+            self.cache_data[summoner_name]["matches_id"] = res
+
+            with open(self.cache_path, "w", encoding="utf-8") as file:
+                file.write(json.dumps(self.cache_data))
+
+        return res
+
+    def getMatchData(self, match_id: str, summoner_name: str = "Phoque éberlué") -> dict:
+        """
+        Gets the matches IDs of a given summoner
+        :param match_id: the match_id of the game
+        :param summoner_name: the name of the summoner
+        :return: match data
+        """
+
+        # Checks if the match data already in cache file
+        if self.use_cache_file \
+                and "matches_data" in self.cache_data[summoner_name] \
+                and match_id in self.cache_data[summoner_name]["matches_data"]:
+            return self.cache_data[summoner_name]["matches_data"][match_id]
+
+        url = f'https://europe.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={self.api_key}'
+
+        res = self.execCurl(url)
+        print(res)
+
+        # Saves match data in cache file
+        if self.use_cache_file:
+            # Create a dictionary for matches data
+            if "matches_data" not in self.cache_data[summoner_name]:
+                self.cache_data[summoner_name]["matches_data"] = {}
+
+            self.cache_data[summoner_name]["matches_data"][match_id] = res
+
+            with open(self.cache_path, "w", encoding="utf-8") as file:
+                file.write(json.dumps(self.cache_data))
+
+        return res
+
+    def getMatchesData(self, matches_id: list[str], summoner_name: str) -> dict:
+        """
+        Gets the data from the matches id passed as parameter
+        :param matches_id: a list of matches id
+        :param summoner_name:  the summoner name
+        :return: dict of matches data
+        """
+        time.sleep(1)
+
+        res = {}
+        for i, match_id in enumerate(matches_id):
+            # Respecting rate limits of riot API
+            if i % 20 == 0:
+                time.sleep(1)
+
+            res[match_id] = self.getMatchData(match_id, summoner_name)
+
+        return res
+
+    # ------------------------- NOT WORKING ----------------------------------
+    def getSummonerAllMatchesID(self, puuid: str, season: int) -> set[str]:
+        """
+        Gets the matches IDs of a given summoner
+        :param puuid: the puuid of the summoner
+        :param season: the year of the season e.g: S12 = 2022
         :return: a list of match ID
         """
         matches_id = set()
 
-        start_date = date.fromisocalendar(2022, 1, 1)
+        start_date = date.fromisocalendar(season, 1, 1)
         start_time = time.mktime(start_date.timetuple())
 
         nb_day_this_season = (date.today() - start_date).days
