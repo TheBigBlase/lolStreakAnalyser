3df7fda7827c5e34093602ef7607872c00455651
BigBlase <thebigblase@gmail.com>
2022-11-01 12:11:52 +0100

    index on get-matches-id: 972db36 'fixed' readme + apikey.json

--- main.py
+++ main.py
@@ -12,15 +12,20 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--noCache", help = "do not use already retrieved data"\
             "(default = false)", action="store_true", default = False)
 
+    parser.add_argument("-f", "--forceCacheReload", help = "make a new cache file (useful for storing data)",\
+            action="store_true", default = False)
+
     args = parser.parse_args()
     ## END ARGUMENT PARSER
 
-    GameExtractor = GameExtractor(use_cache_file = not(args.noCache))
+    GameExtractor = GameExtractor(use_cache_file = not(args.noCache), force_cache_reload = args.forceCacheReload)
 
     summoner_name = GameExtractor.sanitizeSummonerName(args.summonerName)
 
     puuid: str = GameExtractor.getPuuidBySummonerName(summoner_name)
+    #old way
     #matches_id: list[str] = GameExtractor.getSummonerMatchesID(puuid, summoner_name)
     #matches_data: list[any] = GameExtractor.getMatchesData(matches_id, summoner_name)
+    #new way
     matches_ids = GameExtractor.getSummonerAllMatchesID(summoner_name)
     GameExtractor.getMatchesData(matches_ids, summoner_name)
